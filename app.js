1c1093abdaa1bfada5912242d5e0ecb95659bc68
Khairul Bashar Sakib <72920308+4MKBS@users.noreply.github.com>
2022-08-04 15:48:18 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+function inp()  {
+    let a=document.createElement("div");
+    let b=document.createElement("div");
+    let c=document.createElement("div");
+    let d=document.createElement("div");
+    
+    a.innerHTML="আস্তাগফিরুল্লাহ ";
+    b.innerHTML="সুবহানাল্লাহ  ";
+    c.innerHTML="আলহামদুলিল্লাহ ";
+    d.innerHTML="আল্লাহু আকবার ";
+    
+    a.setAttribute("class","forgive") ;
+    b.setAttribute("class","forgive") ;
+    c.setAttribute("class","forgive") ;
+    d.setAttribute("class","forgive") ;
+    
+    document.body.appendChild(a);
+    document.body.appendChild(b);
+    document.body.appendChild(c);
+    document.body.appendChild(d);
+    }
+    setInterval(inp,10);
